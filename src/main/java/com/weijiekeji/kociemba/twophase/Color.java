f6467ed2c1141312d0932c49a2f25a673ff3faa4
litao <410758717@qq.com>
2021-04-22 15:29:16 +0800

    1.0.0

---
+++ src/main/java/com/weijiekeji/kociemba/twophase/Color.java
@@ -0,0 +1,6 @@
+package com.weijiekeji.kociemba.twophase;
+
+//++++++++++++++++++++++++++++++ Names the colors of the cube facelets ++++++++++++++++++++++++++++++++++++++++++++++++
+enum Color {
+	U, R, F, D, L, B
+}
---
+++ src/main/java/com/weijiekeji/tngou/cache/TgCacheEngine.java
@@ -0,0 +1,96 @@
+package com.weijiekeji.tngou.cache;
+
+import com.weijiekeji.tngou.cache.fst.SerializationUtils;
+import com.weijiekeji.tngou.cache.util.FileManager;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * 文件存储换成
+ * @author tngou@tngou.net
+ *
+ */
+public class TgCacheEngine implements CacheEngine {
+
+	private  static FileManager fileManager;
+	static
+	{
+		fileManager= new FileManager();
+		fileManager.init();
+	}
+	@Override
+	public void add(String cacheName, Serializable key, Object value) {
+		if(cacheName!=null&&key!=null&&value!=null){		
+			try {
+				byte[] data = SerializationUtils.serialize(value);
+				fileManager.add(cacheName, key, data);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	@Override
+	public void add( Serializable key, Object value) {
+		add(cacheName, key, value);	
+	}
+	@Override
+	public Object get(String cacheName, Serializable key) {
+		if(cacheName!=null&&key!=null)
+		{
+			try {
+				byte[] data = fileManager.get(cacheName, key);
+				return SerializationUtils.deserialize(data);
+			} catch (IOException e) {			
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+	@Override
+	public Object get( Serializable key) {		
+		return get(cacheName, key);
+	}
+
+	@Override
+	public void remove(String cacheName, Serializable key) {
+		if(cacheName!=null&&key!=null)
+		{
+			fileManager.remove(cacheName, key);
+		}
+		
+	}
+	@Override
+	public void remove( Serializable key) {
+		remove(cacheName, key);
+		
+	}
+	@Override
+	public void clear(String cacheName) {
+		if(cacheName!=null)
+		{
+			try {
+				fileManager.clear(cacheName);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	@Override
+	public void clear() {
+		clear(cacheName);
+		
+	}
+	@Override
+	public void stop() {
+		try {
+			fileManager.close();
+		} catch (IOException e) {		
+			e.printStackTrace();
+		}
+		
+	}
+
+}
